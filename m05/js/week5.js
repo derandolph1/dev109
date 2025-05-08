2ce166e684e27215388969c35f0d71dd78143ef1
derandolph1 <deena.randolph@bellevuecollege.edu>
2025-05-08 03:09:16 -0700

    Create week5.js

---
+++ m05/js/week5.js
@@ -0,0 +1,8 @@
+/*alert:  Welcome user!
+Create a variable message (var message;)
+prompt:  Ask the user for the firstname and store it in variable firstname (var firstname;)
+prompt:  Ask the user for the second name and store it in a variable lastname (var lastname;)
+In the Message concatenate the firstname and lastname  (HINT:  Concatenation in Javascript is done by the + symbol).
+Display message by using:*/
+var message = "Hello!"
+document.write('<h3>' + message + '</h3>');
