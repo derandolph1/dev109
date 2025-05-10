122d81c3f5a5f1c8ef4d2fed2647983cadc5256e
derandolph1 <deena.randolph@bellevuecollege.edu>
2025-05-09 22:47:38 -0700

    Update week5.js
    Use text variable for storing text before printing.

--- m05/js/week5.js
+++ m05/js/week5.js
@@ -3,21 +3,26 @@ DEV109 - M05 Assignment - Multiplication Table
 Deena Randolph
 05/10/25
 */
-var sum = 0
+// Declare product variable and element variable for printing.
+var product = 0
 var el = document.getElementById('blackboard')
-//Display message
-var message = "Multiplication Table"
-el.textContent = '<h2>' + message + '</h2>';
 
-//Prompt user for number
+//Create message variable and add to text
+var message = "Multiplication Table"
+text = '<h2>' + message + '</h2>';
+
+//Prompt user for base number 
 var userInput = prompt('Enter a number from 0-10: ');
-if (Number(userInput) == NaN) {
+if (userInput == null) { //if no number given use 0
   userInput = 0;
 } 
 
-//Loop and print output multiplication table
+//Loop through table and add results to text
 for (var i = 0; i < 11; i++) {
-  sum = userInput * i
-  el.textContent = i + ' x ' + userInput + ' = ' + sum\n;
+  product = userInput * i
+  text += i + ' x ' + userInput + ' = ' + product + '<br>';
 }
+
+//print text to blackboard
+el.innerHTML = text
 
