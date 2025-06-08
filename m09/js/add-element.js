aa162a6d956e754a7923a848a060b936b6fb25c9
derandolph1 <deena.randolph@bellevuecollege.edu>
2025-06-08 03:19:31 -0700

    Update add-element.js
    Update js to run in a function.

--- m09/js/add-element.js
+++ m09/js/add-element.js
@@ -1,14 +1,19 @@
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
-
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
-
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
-
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
-
-// Insert the new element into its position.
-position.appendChild(newEl);
+function addItem(){
+  //Get input value and assign to variable
+  var x = document.getElementById('item').value;
+  
+  // Create a new element and store it in a variable.
+  var newEl = document.createElement('li');
+  
+  // Create a text node and store it in a variable.
+  var newText = document.createTextNode(x);
+  
+  // Attach the new text node to the new element.
+  newEl.appendChild(newText);
+  
+  // Find the position where the new element should be added.
+  var position = document.getElementsByTagName('ul')[0];
+  
+  // Insert the new element into its position.
+  position.appendChild(newEl);
+}
