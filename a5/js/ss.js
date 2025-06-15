d7a0b24a23feeb15d051c40c9ce7333ddba682f4
derandolph1 <deena.randolph@bellevuecollege.edu>
2025-06-14 19:08:40 -0700

    Update ss.js
    Change from static to automatic with a timeout of 4 secs.

--- a5/js/ss.js
+++ a5/js/ss.js
@@ -7,7 +7,26 @@ function plusSlides(n) {
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    /*slides[i].style.display = "none";*/
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+/*function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
@@ -25,4 +44,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
+}*/
